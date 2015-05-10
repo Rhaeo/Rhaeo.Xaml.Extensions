3e6d5b229400eef9bb9ac72772bf60b239fa6405
Tomáš Hübelbauer <tomas@hubelbauer.net>
2015-05-10 18:32:54 +0200

    Generate experimental syntax using Roslyn

--- Sources/Rhaeo.Xaml.Extensions/Rhaeo.Xaml.Roslyn/Program.cs
+++ Sources/Rhaeo.Xaml.Extensions/Rhaeo.Xaml.Roslyn/Program.cs
@@ -1,15 +1,51 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="Program.cs" company="Rhaeo">
+//   Licenced under the MIT licence.
+// </copyright>
+// <summary>
+//   Defines the Program type.
+// </summary>
+// --------------------------------------------------------------------------------------------------------------------
+
+using System;
+using Microsoft.CodeAnalysis;
+using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.Formatting;
 
 namespace Rhaeo.Xaml.Roslyn
 {
-  class Program
+  /// <summary>
+  /// The program.
+  /// </summary>
+  public static class Program
   {
-    static void Main(string[] args)
+    #region Methods
+
+    /// <summary>
+    /// The main method.
+    /// </summary>
+    private static void Main()
     {
+      var classDeclaration = SyntaxFactory.ClassDeclaration("Fatcow16x16Icons");
+
+      var namespaceDeclaration = SyntaxFactory.NamespaceDeclaration(SyntaxFactory.IdentifierName("Rhaeo.Xaml.Extensions")).AddMembers(classDeclaration);
+
+      var compilationUnit =
+        SyntaxFactory.CompilationUnit()
+          .AddUsings(SyntaxFactory.UsingDirective(SyntaxFactory.IdentifierName("System")))
+          .AddMembers(namespaceDeclaration);
+
+      using (var adhocWorkspace = new AdhocWorkspace())
+      {
+        var str = Formatter.Format(compilationUnit, adhocWorkspace);
+        Console.WriteLine(str);
+        Console.WriteLine();
+        Console.WriteLine("Done");
+      }
+
+      Console.ReadLine();
     }
+
+    #endregion
   }
 }
