720266545ea679a2d0047eec41f1226c382bc4be
Tomáš Hübelbauer <tomas@hubelbauer.net>
2015-05-10 20:10:41 +0200

    Figure out how to add an XML documentation trivia

--- Sources/Rhaeo.Xaml.Extensions/Rhaeo.Xaml.Roslyn/Program.cs
+++ Sources/Rhaeo.Xaml.Extensions/Rhaeo.Xaml.Roslyn/Program.cs
@@ -10,6 +10,7 @@
 using System;
 using Microsoft.CodeAnalysis;
 using Microsoft.CodeAnalysis.CSharp;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
 using Microsoft.CodeAnalysis.Formatting;
 
 namespace Rhaeo.Xaml.Roslyn
@@ -29,6 +30,29 @@ namespace Rhaeo.Xaml.Roslyn
       var classDeclaration = SyntaxFactory.ClassDeclaration("Fatcow16x16Icons");
 
       var namespaceDeclaration = SyntaxFactory.NamespaceDeclaration(SyntaxFactory.IdentifierName("Rhaeo.Xaml.Extensions")).AddMembers(classDeclaration);
+      ////namespaceDeclaration.AddMembers(SyntaxFactory.XmlComment
+      ////  (
+      ////  SyntaxFactory.XmlElement(
+      ////    SyntaxFactory.XmlElementStartTag(SyntaxFactory.XmlName("summary")),
+      ////    SyntaxFactory.XmlElementEndTag(SyntaxFactory.XmlName("summary"))
+      ////  )));
+
+      var element =
+        SyntaxFactory.XmlElement(
+          SyntaxFactory.XmlElementStartTag(SyntaxFactory.XmlName("summary")),
+          SyntaxFactory.XmlElementEndTag(SyntaxFactory.XmlName("summary"))
+        ).WithLeadingTrivia(SyntaxFactory.DocumentationCommentExterior("/// "));
+
+      var list = new SyntaxList<XmlNodeSyntax>().Add(element);
+
+      var documentationTrivia = SyntaxFactory.Trivia(
+        SyntaxFactory.DocumentationCommentTrivia(
+          SyntaxKind.MultiLineDocumentationCommentTrivia,
+          list
+        )  
+      );
+
+      namespaceDeclaration = namespaceDeclaration.WithLeadingTrivia(documentationTrivia);
 
       var compilationUnit =
         SyntaxFactory.CompilationUnit()
